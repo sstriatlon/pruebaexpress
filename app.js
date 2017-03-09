dcb858d4791d88d7dffb1d6ed7923603d5a90865
santiago <sstriatlon@gmail.com>
2017-03-09 16:31:59 -0300

    Solucionado problema en el post

--- app.js
+++ app.js
@@ -4,29 +4,25 @@ var app = express();
 var bodyParser = require('body-parser');
 app.set('views', './views')
 app.set('view engine', 'pug');
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());  
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', function(req,res){
 	res.send('Inicio');
 });
 
-/*app.get('/:test', function(req, res){
-	res.render('form', {title: 'Titulo', text: 'Funciona ', test:req.params.test});
-});*/
-
 app.get('/formulario', function(req,res){
-	res.render('formulario', {title: 'Formulario de prueba', header:'Ingrese datos.'});
+	res.render("formulario", {title: 'Formulario de prueba', header:'Ingrese datos.'});
 });
 
-app.post('/formu', function(req,res){
-	//console.log(req.params);
-	app.render('page', {title:'pagina'});
-	//res.send('puta madre');
+app.post('/page', function(req,res){
+
+	var usuario = req.body.user;
+	res.render('page', {user:usuario, title:'Bienvenido'});
+	//res.redirect('/page');
+
 });
 
-app.get('/formu', function(req, res){
-	app.render('page', {title:'pagina'});
-});
+
 
 app.listen(8080);
